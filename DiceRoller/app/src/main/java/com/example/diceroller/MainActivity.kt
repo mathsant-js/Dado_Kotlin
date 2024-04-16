03df3f8f8a92d1a7c3511ba9b2bd85711dc6de35
mathsant-js <matheusjorgesantana114@gmail.com>
2024-04-16 12:10:58 -0300

    Dado_Toast

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -9,7 +10,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -23,6 +26,9 @@ import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +40,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 @Preview
 @Composable
 fun DiceRollerApp() {
@@ -43,7 +48,6 @@ fun DiceRollerApp() {
         .wrapContentSize(Alignment.Center)
     )
 }
-
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     var result by remember { mutableStateOf(1) }
@@ -55,6 +59,8 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
+    var number by remember {(mutableStateOf(""))}
+    val context = LocalContext.current
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -64,8 +70,23 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
             contentDescription = result.toString()
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result = (1..6).random() }) {
-            Text(stringResource(R.string.roll))
+        Button(onClick = {
+            result = (1..6).random()
+            if (number.toInt() == result){
+                Toast.makeText(context, "Você venceu, parabéns!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "Você perdeu, tente de novo.", Toast.LENGTH_SHORT).show()
+            }
+        }) {
+            Text(stringResource(R.string.roll), fontSize = 24.sp)
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = number,
+            onValueChange = {number = it},
+            label = {Text("Número sorteado")},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
     }
 }
